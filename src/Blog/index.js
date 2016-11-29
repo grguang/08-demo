307d087e7d48e0ed6033c38bd4fff12128c102c9
grguang <945455382@qq.com>
2016-11-29 11:51:29 +0800

    html

---
+++ src/Blog/index.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react'
+
+class Blog extends React.Component {
+  render () {
+    return(
+      <div>
+        展示Blog
+      </div>
+    )
+  }
+}
+
+export default Blog;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import App from './App.js';
+import './main.css'
+import Routers from './Routers.js'
 
-render(<App />,document.getElementById('root'));
+render(
+  <Routers />
+  ,document.getElementById('root'));
