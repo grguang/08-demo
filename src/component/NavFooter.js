020f2b782ac65b8927eb7323f80d9a6cffaaf1bd
grguang <945455382@qq.com>
2016-11-30 09:12:34 +0800

    add

--- src/component/NavFooter.js
+++ src/component/NavFooter.js
@@ -1,10 +1,15 @@
 import React, { PropTypes } from 'react'
+import {Link} from 'react-router'
 
 class NavFooter extends React.Component {
   render () {
     return(
-      <div>
-        footer
+      <div className="nav-footer">
+        <Link activeStyle={{color:'purple'}} onlyActiveOnIndex={true } to='/'><span className="glyphicon glyphicon-home" aria-hidden="true"></span><br />Home</Link>
+        <Link activeStyle={{color:'purple'}} to='blog'><span className="glyphicon glyphicon-th-large" aria-hidden="true"></span><br />Blog</Link>
+        <Link activeStyle={{color:'purple'}} to="work"><span className="glyphicon glyphicon-book" aria-hidden="true"></span><br />Work</Link>
+        <Link activeStyle={{color:'purple'}} to='about'><span className="glyphicon glyphicon-user" aria-hidden="true"></span><br />About</Link>
+
       </div>
     )
   }
--- src/component/NavHeader.js
+++ src/component/NavHeader.js
@@ -3,8 +3,14 @@ import React, { PropTypes } from 'react'
 class NavHeader extends React.Component {
   render () {
     return(
-      <div>
-        NavHeader
+      <div className='nav-header'>
+        <button type="button" className="btn btn-default btn-lg">
+          <span className="glyphicon glyphicon-xbt" aria-hidden="true"></span>ack
+        </button>
+        <h3>Grguang@</h3>
+        <button type="button" className="btn btn-default btn-lg">
+          <span className="glyphicon glyphicon-globe" aria-hidden="true"></span>
+        </button>
       </div>
     )
   }
