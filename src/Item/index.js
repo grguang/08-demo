6955c0d59c941b699df4dbd51358aff93cdf980f
grguang <945455382@qq.com>
2016-12-03 16:30:38 +0800

    item

--- src/Item/index.js
+++ src/Item/index.js
@@ -1,16 +1,27 @@
 import React, { PropTypes } from 'react'
 
 import marked from 'marked'
+import axios from 'axios'
+
+import Loading from '../component/Loading.js'
 
 class Item extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      data:'',
+      wait:true
+    }
+  }
+  componentDidMount(){
+    let address = this.props.params.title
+    axios.get(`https://raw.githubusercontent.com/grguang/08-demo/master/data/${address}.md`)
+      .then(res=>this.setState({data:res.data,wait:false}))
+  }
   render () {
-    let content = this.props.params.title==0 ? 'yi' :
-      this.props.params.title==1 ? 'er' :
-        this.props.params.title==2 ? 'san' : 'si'
     return(
-      <div>
-        {content}
-        <div dangerouslySetInnerHTML={{__html:marked('# 我是h1')}}></div>
+      <div className="item-wrap">
+        {this.state.wait ? <Loading /> : <div dangerouslySetInnerHTML={{__html:marked(this.state.data)}}></div>}
       </div>
     )
   }
