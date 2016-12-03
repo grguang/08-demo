38b77c16e1b6bcf818f70fcbb85e520e81ece9b5
grguang <945455382@qq.com>
2016-12-03 15:43:52 +0800

    md

--- src/Item/index.js
+++ src/Item/index.js
@@ -1,13 +1,16 @@
 import React, { PropTypes } from 'react'
 
+import marked from 'marked'
+
 class Item extends React.Component {
   render () {
-    let content = this.props.params.title==1 ? 'yi' :
-      this.props.params.title==2 ? 'er' :
-        this.props.params.title==3 ? 'san' : 'si'
+    let content = this.props.params.title==0 ? 'yi' :
+      this.props.params.title==1 ? 'er' :
+        this.props.params.title==2 ? 'san' : 'si'
     return(
       <div>
         {content}
+        <div dangerouslySetInnerHTML={{__html:marked('# 我是h1')}}></div>
       </div>
     )
   }
